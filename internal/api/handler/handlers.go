6d5769815ebf9602e8225db9ed0414eb75764559
NuwoMaaan <Leoholden24@outlook.com>
2026-02-18 17:18:15 +1100

    add remaining routes

--- internal/api/handler/handlers.go
+++ internal/api/handler/handlers.go
@@ -19,7 +19,79 @@ func (h *ArcherHandler) GetArchersByID(w http.ResponseWriter, r *http.Request) {
 func (h *ArcherHandler) GetArchersScoreAboveValue(w http.ResponseWriter, r *http.Request) {
 	result, err := repository.GetArcherScoreAboveValue(h.db)
 	if err != nil {
-		http.Error(w, "Failed to retrieve archer", http.StatusInternalServerError)
+		http.Error(w, "Failed to retrieve", http.StatusInternalServerError)
+		return
+	}
+	_ = json.NewEncoder(w).Encode(result)
+}
+
+func (h *ArcherHandler) GetChampionScoresByEvenetID(w http.ResponseWriter, r *http.Request) {
+	result, err := repository.GetChampionScoresByEventID(h.db)
+	if err != nil {
+		http.Error(w, "Failed to retrieve", http.StatusInternalServerError)
+		return
+	}
+	_ = json.NewEncoder(w).Encode(result)
+}
+
+func (h *ArcherHandler) GetEndCountOrderTotalByDate(w http.ResponseWriter, r *http.Request) {
+	result, err := repository.GetEndCountOrderTotalByDate(h.db)
+	if err != nil {
+		http.Error(w, "Failed to retrieve", http.StatusInternalServerError)
+		return
+	}
+	_ = json.NewEncoder(w).Encode(result)
+}
+
+func (h *ArcherHandler) GetEndCountOrderTotalByTotal(w http.ResponseWriter, r *http.Request) {
+	result, err := repository.GetEndCountOrderTotalByTotal(h.db)
+	if err != nil {
+		http.Error(w, "Failed to retrieve", http.StatusInternalServerError)
+		return
+	}
+	_ = json.NewEncoder(w).Encode(result)
+}
+
+func (h *ArcherHandler) GetEndCountTotal(w http.ResponseWriter, r *http.Request) {
+	result, err := repository.GetEndCountTotal(h.db)
+	if err != nil {
+		http.Error(w, "Failed to retrieve", http.StatusInternalServerError)
+		return
+	}
+	_ = json.NewEncoder(w).Encode(result)
+}
+
+func (h *ArcherHandler) GetEndCountTotalByRoundNum(w http.ResponseWriter, r *http.Request) {
+	result, err := repository.GetEndCountOrderTotalByRoundNum(h.db)
+	if err != nil {
+		http.Error(w, "Failed to retrieve", http.StatusInternalServerError)
+		return
+	}
+	_ = json.NewEncoder(w).Encode(result)
+}
+
+func (h *ArcherHandler) GetPersonalBestAllArchers(w http.ResponseWriter, r *http.Request) {
+	result, err := repository.GetPersonalBestAllArchers(h.db)
+	if err != nil {
+		http.Error(w, "Failed to retrieve", http.StatusInternalServerError)
+		return
+	}
+	_ = json.NewEncoder(w).Encode(result)
+}
+
+func (h *ArcherHandler) GetPersonalBestByRound(w http.ResponseWriter, r *http.Request) {
+	result, err := repository.GetPersonalBestByRound(h.db)
+	if err != nil {
+		http.Error(w, "Failed to retrieve", http.StatusInternalServerError)
+		return
+	}
+	_ = json.NewEncoder(w).Encode(result)
+}
+
+func (h *ArcherHandler) GetRoundAndCategoryDetails(w http.ResponseWriter, r *http.Request) {
+	result, err := repository.GetRoundAndCategoryDetails(h.db)
+	if err != nil {
+		http.Error(w, "Failed to retrieve", http.StatusInternalServerError)
 		return
 	}
 	_ = json.NewEncoder(w).Encode(result)
--- internal/api/routes.go
+++ internal/api/routes.go
@@ -25,6 +25,16 @@ func LoadRoutes(db *sql.DB) *chi.Mux {
 func loadArcherClubRoutes(router chi.Router, db *sql.DB) {
 	ArcherClubHandler := handler.NewArcherHandler(db)
 
-	router.Get("/", ArcherClubHandler.GetArchersByID)
+	// SQL Queries are predefined and don't take parameters, therefore no URL parameters for something like /ArcheryClub/get-archer/{id}
+	// Not ideal for real API but serves the purpose of demonstrating db interaction via http
+	router.Get("/get-archer", ArcherClubHandler.GetArchersByID)
 	router.Get("/score-above", ArcherClubHandler.GetArchersScoreAboveValue)
+	router.Get("/champion-scores", ArcherClubHandler.GetChampionScoresByEvenetID)
+	router.Get("/score-by-date", ArcherClubHandler.GetEndCountOrderTotalByDate)
+	router.Get("/score-by-total", ArcherClubHandler.GetEndCountOrderTotalByTotal)
+	router.Get("/end-count-total", ArcherClubHandler.GetEndCountTotal)
+	router.Get("/score-by-roundnum", ArcherClubHandler.GetEndCountTotalByRoundNum)
+	router.Get("/round-category-details", ArcherClubHandler.GetRoundAndCategoryDetails)
+	router.Get("/personal-best-by-round", ArcherClubHandler.GetPersonalBestByRound)
+	router.Get("/personal-best-all-archers", ArcherClubHandler.GetPersonalBestAllArchers)
 }
