a17aff43deb689db5c6b1e470eb138a8712cd127
NuwoMaaan <Leoholden24@outlook.com>
2026-02-18 18:54:30 +1100

    add middleware logger

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -6,11 +6,13 @@ import (
 
 	"github.com/NuwoMaaan/Archery-MySQL-Database/internal/api/handler"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func LoadRoutes(db *sql.DB) *chi.Mux {
 	router := chi.NewRouter()
 
+	router.Use(middleware.Logger)
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
